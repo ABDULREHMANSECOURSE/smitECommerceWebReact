24e5366110de375fcce96141ea1589c789e7150b
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-03 14:12:04 +0500

    .

---
+++ src/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/header/navbar'
+import Hero from '../components/header/Hero'
+import Footer from '../components/footer'
+
+export const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <Footer />
+        </>
+    )
+}
